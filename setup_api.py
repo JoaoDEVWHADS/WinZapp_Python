15ed8b8476d2ce37bc3c88c4f41abbda5b050e3f
JoaoDEVWHADS <121991282+JoaoDEVWHADS@users.noreply.github.com>
2026-07-02 14:09:34 +0000

    Revert "chore: add client/api/.gitignore to setup_api.py custom files and unignore it in main gitignore"
    This reverts commit 2c0d37b1ca7273a2e9fc3313dfecb4f74b93112c.

--- setup_api.py
+++ setup_api.py
@@ -84,7 +84,6 @@ def main():
         
         # Additional custom files to backup and restore
         custom_files = [
-            ".gitignore",
             "src/config.ts",
             "src/util/createSessionUtil.ts",
             "src/util/functions.ts",
