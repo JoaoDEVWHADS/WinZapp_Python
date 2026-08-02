6fdbfd5a9b328bfed26cb5c3673bbdc980efa414
JoaoDEVWHADS <121991282+JoaoDEVWHADS@users.noreply.github.com>
2026-08-02 15:14:44 +0000

    fix(api): preserve dist/index.js in patch files to eliminate ./index require warning

--- setup_api.py
+++ setup_api.py
@@ -47,6 +47,7 @@ CUSTOM_SRC_FILES = [
     "src/controller/sessionController.ts",
     "src/routes/index.ts",
     "dist/controller/sessionController.js",
+    "dist/index.js",
     "decrypt.js",
 ]
 
