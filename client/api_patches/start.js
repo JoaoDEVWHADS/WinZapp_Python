ac90b18b0bc96ada4e5fe1ee4e2d4b22fb673fdc
JoaoDEVWHADS <121991282+JoaoDEVWHADS@users.noreply.github.com>
2026-09-16 19:00:18 -0300

    fix: stop Chromium from transmitting fake microphone

--- client/api_patches/start.js
+++ client/api_patches/start.js
@@ -297,7 +297,6 @@ const optimizedBrowserArgs = [
   '--metrics-recording-only',
   '--autoplay-policy=no-user-gesture-required',
   '--use-fake-ui-for-media-stream',
-  '--use-fake-device-for-media-stream',
   '--mute-audio',
   '--no-first-run',
   '--safebrowsing-disable-auto-update',
